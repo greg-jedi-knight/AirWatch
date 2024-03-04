28dbb98929bf3a8d2a91088b8dc0b55ed96568bc
greg <greg99.fordon@gmail.com>
2024-03-04 07:07:44 +0000

    Listing projects added

--- airwatch/scripts/test.py
+++ airwatch/scripts/test.py
@@ -1,101 +1,3 @@
-from oauthlib.oauth2 import BackendApplicationClient
-from requests_oauthlib import OAuth2Session
-import requests
-import math
-
-def epsg3857toEpsg4326(pos):
-  x = pos[0]
-  y = pos[1]
-
-  x = (x * 180) / 20037508.34
-  y = (y * 180) / 20037508.34
-  y = (math.atan(math.pow(math.e, y * (math.pi / 180))) * 360) / math.pi - 90
-
-  return [x, y]
-
-EVALSCRIPT = """
-
-//VERSION=3
-
-function setup() {
-  return {
-    input: [{
-      bands: [
-        "CO",
-        "dataMask"
-      ]
-    }],
-    output: [
-      {
-        id: "output_co",
-        bands: 1,
-        sampleType: "FLOAT32"
-      },
-      {
-        id: "dataMask",
-        bands: 1      
-      }
-    ]
-  }
-}
-
-function evaluatePixel(samples) {
-  return {
-    output_co: [samples.CO],
-    dataMask: [samples.dataMask]
-  }
-}
-
-"""
-
-client_id = 'd1e2eebf-0e43-40f2-a64b-ceca033f679c'
-client_secret = 'F41cuO5xBkxrXasa3EZc2Ykv79Nxa6X5'
-
-client = BackendApplicationClient(client_id=client_id)
-oauth = OAuth2Session(client=client)
-
-token = oauth.fetch_token(token_url='https://services.sentinel-hub.com/auth/realms/main/protocol/openid-connect/token',
-                          client_secret=client_secret, include_client_id=True)
-
-headers = {
-  'Content-Type': 'application/json',
-  'Accept': 'application/json'
-}
-
-json = {
-  'input': {
-    'bounds': {
-      'properties': {
-        'crs': 'http://www.opengis.net/def/crs/EPSG/0/3857'
-      },
-      'bbox': [
-        114.4529,
-        113.8259,
-        22.561949,
-        22.1435
-      ]
-    },
-    'data': [{
-      'type': 'sentinel-5p-l2'
-    }]  
-  },
-  'aggregation': {
-    'timeRange': {
-      'from': '2023-08-24T14:15:22Z',
-      'to': '2023-09-20T14:15:22Z'
-    },
-    'aggregationInterval': {
-      'of': 'P20D'
-    },
-    'evalscript': EVALSCRIPT
-  }
-}
-
-url = 'https://creodias.sentinel-hub.com/api/v1/statistics'
-response = oauth.request("POST", url=url , headers=headers, json=json)
-print(response.json())
-
-
 from sentinelhub import (
   BBox,
   CRS,
@@ -111,16 +13,26 @@ EVALSCRIPT = """
 function setup() {
   return {
     input: [{
-      bands: ['CO']
+      bands: ['CO', 'dataMask']
     }],
-    output: {
-      bands: 1
-    }
+    output: [
+      {
+        id: 'output_co',
+        bands: 1
+      },
+      {
+        id: 'dataMask',
+        bands: 1
+      }
+    ]
   };
 }
 
 function evaluatePixel(sample) {
-  return [sample.CO]
+  return {
+      output_co: [sample.CO],
+      dataMask: [sample.dataMask]
+  }
 }
 
 """
@@ -142,7 +54,7 @@ class SentinelClient:
 
     input_data=[SentinelHubStatistical.input_data(DataCollection.SENTINEL5P, maxcc=0.8)]
 
-    bbox = BBox(coordinates, CRS.POP_WEB)
+    bbox = BBox(coordinates, CRS.WGS84)
 
     request = SentinelHubStatistical(
       aggregation=aggregation,
@@ -156,9 +68,9 @@ class SentinelClient:
 def run():
   client_id = 'd1e2eebf-0e43-40f2-a64b-ceca033f679c'
   client_secret = 'F41cuO5xBkxrXasa3EZc2Ykv79Nxa6X5'
-  time_from = '2020-06-07'
-  time_to = '2020-06-13'
-  coordinates = (52.097877, 20.85129, 52.367999, 21.271098)
+  time_from = '2024-02-17'
+  time_to = '2024-02-18'
+  coordinates = (113.8259, 22.1435, 114.4529, 22.561949)
 
   client = SentinelClient(client_id, client_secret)
   data = client.get_air_pollution_data(coordinates, time_from, time_to)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
+from workspaces.models import Project
+
 @require_http_methods(['GET'])
 def main_page_view(request):
 	return render(request, 'dashboard/index.html', {})
 
 @require_http_methods(['GET'])
 def home_page_view(request):
-	return render(request, 'dashboard/dashboard.html', {})	
+	user = request.user
+
+	projects = Project.objects.filter(
+		owner__id=user.id
+	).values('id', 'name')
+
+	context = {
+		'projects': projects
+	}
+
+	return render(request, 'dashboard/dashboard.html', context)	
